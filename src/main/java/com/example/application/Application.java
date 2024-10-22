9bbc5c0047d30dc614e8f8f7bd2f39d9f4d5e37a
kareem medhat <117598008+KareemMedhatSe@users.noreply.github.com>
2024-10-22 23:17:20 +0300

    first commit

---
+++ src/main/java/com/example/application/Application.java
@@ -0,0 +1,27 @@
+package com.example.application;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.theme.Theme;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
+
+
+@SpringBootApplication
+@Push
+@Theme("chat-theme")
+public class Application implements AppShellConfigurator {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
